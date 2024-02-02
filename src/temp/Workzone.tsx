fb02cbad2dea36d25c9b5dc209d0c1fa5f374982
Baliberdin Igor <bivfloart@gmail.com>
2024-02-02 17:06:24 +0200

    delete trash

--- src/temp/Workzone.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = { children: React.ReactNode }
-
-export default function Workzone({ children }: Props) {
-    return (
-        <div className='p-5 bg-gray min-h-screen'>{children}</div>
-    )
-}
